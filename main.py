ca2b25af52b3a64418aaf7286696521054e9ac11
Meenakshy Bindu Suresh <35546428-bsmeenakshy369@users.noreply.replit.com>
2024-08-14 08:14:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Marvel Movie Character Creator")
+print("--")
+spiderMan = input("Do you like 'hanging around'?: ")
+if spiderMan == "Yes":
+  print("Whoa! It's the friendly neighborhood Spider-Man! Web-slinging through the day, I see!")
+else:
+  print("Then you're not Spider-man")
+korg = input("Do you have a 'gravelly' voice?:")
+if korg == "Yes":
+  print("Hey, mate! It's Korg, the rock star of the galaxy! Ready for another revolution, huh?")
+else:
+  print( "Aww, then you're not Korg")
+captainMarvel = input("Do you often feel 'Marvelous?")
+if captainMarvel == "Yes":
+  print("Aha! You're Captain Marvel! Hi!")
+else:
+  print("What on earth are you!!")
+  
